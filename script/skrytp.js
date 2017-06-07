467226a146fb0b66c7420abf778cee4399179b8a
grabowskipawel <grabowskipawel91@gmail.com>
2017-06-07 13:41:24 +0200

    Add files via upload

---
+++ script/skrytp.js
@@ -0,0 +1,17 @@
+
+window.onload = function (){ 
+
+ var button2 = document.getElementById("button2");
+    
+function showAlert()
+ {
+ alert("Formularz zostały wysłany! Dziękujemy.")
+ };
+
+ button2.onclick = showAlert;      
+    
+    
+      
+
+    
+}
